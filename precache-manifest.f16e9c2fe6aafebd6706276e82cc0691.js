7a32b23dce9eeb694c69693928e48b82773f3698
lewisd1996 <lewis.d1996@gmail.com>
2020-03-31 19:57:52 +0100

    Updates

---
+++ precache-manifest.f16e9c2fe6aafebd6706276e82cc0691.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1abef2d1871a838615109ad39accb3ed",
+    "url": "/surferspotter/index.html"
+  },
+  {
+    "revision": "c8a27dcb48ed650ac283",
+    "url": "/surferspotter/static/css/2.131a0933.chunk.css"
+  },
+  {
+    "revision": "fc0c08034840d812341c",
+    "url": "/surferspotter/static/css/main.db9bdf86.chunk.css"
+  },
+  {
+    "revision": "c8a27dcb48ed650ac283",
+    "url": "/surferspotter/static/js/2.13fcd5f5.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/surferspotter/static/js/2.13fcd5f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc0c08034840d812341c",
+    "url": "/surferspotter/static/js/main.ab04bdda.chunk.js"
+  },
+  {
+    "revision": "2deb9f0717933056ff9d",
+    "url": "/surferspotter/static/js/runtime-main.b9601839.js"
+  }
+]);
